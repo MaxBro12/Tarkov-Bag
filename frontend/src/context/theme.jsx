c412f59b70e23a852e7bc3744874e7dfb65e89f8
Maxim Bodrov <activer12@mail.ru>
2026-05-11 12:19:25 +0300

    fix: theme provider

--- frontend/src/context/theme.jsx
+++ frontend/src/context/theme.jsx
@@ -1,11 +1,11 @@
-import { createContext, useState, useContext } from 'react';
+import {createContext, useState, useContext, useEffect} from 'react';
 
 
 const ThemeContext = createContext({});
 
 
 export function ThemeProvider({ children }) {
-    const [theme, setTheme] = useState(get_theme());
+    const [theme, setTheme] = useState('dark');
 
     function get_theme() {
         const them = localStorage.getItem('theme')
@@ -20,6 +20,10 @@ export function ThemeProvider({ children }) {
         setTheme(theme);
         localStorage.setItem('theme', theme);
     }
+
+    useEffect(() => {
+        get_theme()
+    }, []);
 
     return (
         <ThemeContext.Provider value={{
